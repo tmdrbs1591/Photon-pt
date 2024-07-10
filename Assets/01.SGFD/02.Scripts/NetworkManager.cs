6fd8d12d9e608f5818b57dae5a1b2911393d5095
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-07-10 23:05:53 +0900

    TEXT UNDER

--- Assets/01.SGFD/02.Scripts/NetworkManager.cs
+++ Assets/01.SGFD/02.Scripts/NetworkManager.cs
@@ -229,19 +229,15 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
     void ChatRPC(string msg)
     {
-        bool isInput = false;
-        for (int i = 0; i < ChatText.Length; i++)
-            if (ChatText[i].text == "")
-            {
-                isInput = true;
-                ChatText[i].text = msg;
-                break;
-            }
-        if (!isInput) // 꽉차면 한칸씩 위로 올림
+        // 채팅 텍스트들을 위로 밀기
+        for (int i = 0; i < ChatText.Length - 1; i++)
         {
-            for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
-            ChatText[ChatText.Length - 1].text = msg;
+            ChatText[i].text = ChatText[i + 1].text;
         }
+
+        // 가장 아래의 텍스트에 새로운 메시지 추가
+        ChatText[ChatText.Length - 1].text = msg;
     }
+
     #endregion
 }
