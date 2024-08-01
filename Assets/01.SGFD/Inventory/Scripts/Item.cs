f3ff23aaf7af28d1dc60809da3f23fc0c36b21af
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-08-01 23:11:41 +0900

    Fix | inventory

--- Assets/01.SGFD/Inventory/Scripts/Item.cs
+++ Assets/01.SGFD/Inventory/Scripts/Item.cs
@@ -11,11 +11,12 @@ public class Item : MonoBehaviour
     [field: SerializeField]
     public int Quantity { get; set; } = 1;
 
-    [SerializeField]
-    private AudioSource audioSource;
 
     [SerializeField]
     private float duration = 0.3f;
+
+    [SerializeField]
+    private int songIndex;
 
     private void Start()
     {
@@ -29,7 +30,7 @@ public class Item : MonoBehaviour
     }
     private IEnumerator AnimateItemPickup()
     {
-        audioSource.Play();
+        SingleAudioManager.instance.PlaySound(transform.position, songIndex, Random.Range(1f, 1f), 0.4f);
         Vector3 startScale = transform.localScale;
         Vector3 endScale = Vector3.zero;
         float currentTime = 0;
