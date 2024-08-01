d3371cc52cfd1330dda7e87edbed1aa437952e6c
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-08-01 21:35:21 +0900

    Fix | inventory

--- Assets/01.SGFD/Inventory/Scripts/UIInventoryItem.cs
+++ Assets/01.SGFD/Inventory/Scripts/UIInventoryItem.cs
@@ -6,86 +6,89 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIInventoryItem : MonoBehaviour, IPointerClickHandler,
-    IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
+namespace Inventory.UI
 {
-    [SerializeField]
-    private Image itemImage;
-
-    [SerializeField]
-    private TMP_Text quantityTxt;
-
-    [SerializeField]
-    private Image borderImage;
-
-    public event Action<UIInventoryItem> OnItemClicked,
-        OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
-        onRightMouseBtnClick;
-
-    private bool empty = true;
-
-    private void Awake()
+    public class UIInventoryItem : MonoBehaviour, IPointerClickHandler,
+        IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
     {
-        ResetData();
-        Deselect();
+        [SerializeField]
+        private Image itemImage;
 
-    }
-    public void ResetData()
-    {
-        this.itemImage.gameObject.SetActive(false);
-        empty = true;
-    }
+        [SerializeField]
+        private TMP_Text quantityTxt;
 
-    public void Deselect()
-    {
-        borderImage.enabled = false;
-    }
-    public void SetData(Sprite sprite, int quantity)
-    {
-        this.itemImage.gameObject.SetActive(true);
-        this.itemImage.sprite = sprite;
-        this.quantityTxt.text = quantity + "";
-        empty = false;
-    }
-    public void Select()
-    {
-        borderImage.enabled = true;
-    }
+        [SerializeField]
+        private Image borderImage;
 
+        public event Action<UIInventoryItem> OnItemClicked,
+            OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag,
+            onRightMouseBtnClick;
 
+        private bool empty = true;
 
-
-    public void OnPointerClick(PointerEventData pointerData)
-    {
-        if (pointerData.button == PointerEventData.InputButton.Right)
+        private void Awake()
         {
-            onRightMouseBtnClick?.Invoke(this);
+            ResetData();
+            Deselect();
+
         }
+        public void ResetData()
         {
-            OnItemClicked.Invoke(this);
+            itemImage.gameObject.SetActive(false);
+            empty = true;
         }
-    }
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        if (empty)
-            return;
-        OnItemBeginDrag?.Invoke(this);
-    }
+        public void Deselect()
+        {
+            borderImage.enabled = false;
+        }
+        public void SetData(Sprite sprite, int quantity)
+        {
+            itemImage.gameObject.SetActive(true);
+            itemImage.sprite = sprite;
+            quantityTxt.text = quantity + "";
+            empty = false;
+        }
+        public void Select()
+        {
+            borderImage.enabled = true;
+        }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        OnItemEndDrag.Invoke(this);
 
-    }
 
-    public void OnDrop(PointerEventData eventData)
-    {
-        OnItemDroppedOn?.Invoke(this);
 
-    }
+        public void OnPointerClick(PointerEventData pointerData)
+        {
+            if (pointerData.button == PointerEventData.InputButton.Right)
+            {
+                onRightMouseBtnClick?.Invoke(this);
+            }
+            {
+                OnItemClicked.Invoke(this);
+            }
+        }
 
-    public void OnDrag(PointerEventData eventData)
-    {
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (empty)
+                return;
+            OnItemBeginDrag?.Invoke(this);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            OnItemEndDrag.Invoke(this);
+
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            OnItemDroppedOn?.Invoke(this);
+
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+        }
     }
 }
